b82a9dacc2d0ed0bf16d9a558b06c0635906c034
ACTea1 <132441436+ACTea1@users.noreply.github.com>
2023-12-25 19:11:47 +0100

    Update md2xml.py

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,6 +17,29 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
+
+    paragraphs = mddata.split('\n\n')
+    structured_paragraphs = []
+
+    # Patterns for paragraph classification
+    patterns = {
+        "Entête": r"Pourvoi|COUR DE CASSATION|AU NOM DU PEUPLE FRANÇAIS",
+        "Exposé du litige": r"a formé le pourvoi",
+        "Motivation": r"Sur le rapport de",
+        "Moyens": r"\d\.",
+        "Dispositif": r"la Cour :"
+    }
+
+    # Classifying paragraphs and wrapping them in XML
+    current_section = "Entête"
+    for paragraph in paragraphs:
+        for section, pattern in patterns.items():
+            if re.search(pattern, paragraph):
+                current_section = section
+                break
+    structured_paragraphs.append(f'<div class="{current_section}">{paragraph}</div>')
+
+    mddata = '\n'.join(structured_paragraphs)
     xmldata = mddata
 
     xml = list()
