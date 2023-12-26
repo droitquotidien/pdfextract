4fd6ed8a52d466418258e56359ddb7b9f7778e99
Adrian RAMIREZ <jean-piere-adrian.ramirez_caldas@mines-paristech.fr>
2023-12-26 21:31:49 +0100

    extracting pourvoi number and date from txt file

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -4,7 +4,6 @@ Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
 import argparse
 import re
 
-
 def main():
     parser = argparse.ArgumentParser("text2md")
     parser.add_argument('in_file', help="Text file", type=str)
@@ -16,13 +15,22 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    search = re.search(".*", textdata)
+    mddata = search.group(0)
+    
+    pourvoi_search = re.search("Pourvoi n° (.*)", textdata)
+    NUM_POURVOI = pourvoi_search.group(1)
+
+    date_search = re.search("[1-9]?[0-9] .* 20[0-9][0-9]", textdata)
+    DATE = date_search.group(0)
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {NUM_POURVOI} du {DATE}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+main()
