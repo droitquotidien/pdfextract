3b3f9f5347806a893390a5668a878f8b21ad6b4c
tgula <tetiana.gula@etu.minesparis.psl.eu>
2023-12-26 12:32:05 +0100

    TP1 Tetiana Gula

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -14,6 +14,27 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         mddata = f.read()
 
+
+    # Entête
+    entete_match = re.search(r'^.*?AU NOM DU PEUPLE FRANÇAIS', mddata, re.DOTALL | re.MULTILINE)
+    entete_zone = entete_match.group(0) if entete_match else ''
+
+    # Exposé du litige
+    expose_match = re.search(r'(?<=AU NOM DU PEUPLE FRANÇAIS)(.*?)(?=\s*Sur le rapport\b)', mddata, re.DOTALL | re.MULTILINE)
+    expose_zone = expose_match.group(0) if expose_match else ''
+
+    # Motivation
+    motivation_match = re.search(r'Sur le rapport(.*?)(?=\s*(Faits et procédure|\b1\b))', mddata, re.DOTALL | re.MULTILINE)
+    motivation_zone = motivation_match.group(0) if motivation_match else ''
+
+    # Moyens 
+    moyens_match = re.search(r'(\s*(Faits et procédure|1\.)(.*?)(?=\s*(PAR CES MOTIFS|EN CONSÉQUENCE|PAR CES MOTIFS)))', mddata, re.DOTALL | re.MULTILINE)
+    moyens_zone = moyens_match.group(0) if moyens_match else ''
+
+    # Dispositif 
+    dispositif_match = re.search(r'\s*(PAR CES MOTIFS|EN CONSÉQUENCE|PAR CES MOTIFS)([\s\S]*)$', mddata, re.DOTALL | re.MULTILINE)
+    dispositif_zone = dispositif_match.group(0) if dispositif_match else ''
+
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
@@ -22,7 +43,12 @@ def main():
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    xml.append('<Entête>' + entete_zone + '</Entête>')
+    xml.append('<Exposé_du_litige>' + expose_zone + '</Exposé_du_litige>')
+    xml.append('<Motivation>' + motivation_zone + '</Motivation>')
+    xml.append('<Moyens>' + moyens_zone + '</Moyens>')
+    xml.append('<Dispositif>' + dispositif_zone + '</Dispositif>')
+    #xml.append(xmldata)
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -4,6 +4,29 @@ Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
 import argparse
 import re
 
+def process_lines(textdata):
+    lines = textdata.split('\n')
+
+    processed_lines = []
+    current_line = ""
+
+    for line in lines:
+        if not line.strip():
+            continue
+
+        if re.match(r'^[A-Z]', line) or re.match(r'^\d', line):
+            if current_line:
+                processed_lines.append(current_line)
+            current_line = line
+        else:
+            current_line += ' ' + line
+
+    if current_line:
+        processed_lines.append(current_line)
+
+    result = '\n'.join(processed_lines)
+    return result
+
 
 def main():
     parser = argparse.ArgumentParser("text2md")
@@ -14,9 +37,28 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
+    textdata = re.sub(r'Page \d+ / \d+\n(.*)', '', textdata)
+    textdata = re.sub(r'Page \d+ / \d+', '', textdata)
+
+    # deleting the line that starts with smth but ands with a date: 1st approach, a stupid one..
+    # unwanted_pourvoi = re.compile(r'^.*?(\d{1,2} [^\s]+ \d{4})$', re.MULTILINE)
+    # matches = unwanted_pourvoi.finditer(textdata)
+    # unique_dates = set()
+
+    # for match in matches:
+    #     date = match.group(1)
+    #     if date not in unique_dates:
+    #         unique_dates.add(date)
+    #     else:
+    #         textdata = textdata.replace(match.group(0), '')
+
+    processed_text = process_lines(textdata)
+
+    mddata = processed_text
+
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+
 
     md = list()
     md.append("# Pourvoi NUM_POURVOI du DATE")
