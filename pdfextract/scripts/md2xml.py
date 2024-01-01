afe64b0d6de31a584fa25aedb40b9bdb03fcecac
aseran01 <serandourantoine@gmail.com>
2024-01-01 19:12:49 +0100

    TD1 2 python scripts txt in md in xml, commented

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,14 +17,69 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    
+    var = re.split(r'(DU \d+ \w* 2023)',mddata)     # on remplit l'entete si on a la mention de la date
+    if len(var)==3:
+        entete, exp_reg , txt_suite = var
+        entete = entete + exp_reg
+        mddata = txt_suite
+    else:
+        entete = ''
+    
+    var = re.split(r'(Sur le rapport)',mddata)      # on remplit l'expose si on a la mention "sur le rapport", début de la partie motivation
+    if len(var)==3:
+        expose, exp_reg , txt_suite = var
+        expose = expose 
+        mddata = exp_reg + txt_suite 
+    else:
+        expose = ''
+    
+    var = re.split(r'(Examen d.*moyens?)',mddata)   # on teste si on a examen des moyens, si oui la motiv est rempli jusqu'à la section moyen, sinon on n'a pas de moyens
+    if len(var)==3:
+        motiv, exp_reg , txt_suite = var
+        motiv = motiv 
+        mddata = exp_reg + txt_suite 
+        moyens = 'en attente'
+    else:
+        moyens = ''
+    
+    var = re.split(r'(PAR CES MOTIFS.*la Cour :|EN CONSÉQUENCE.*la Cour :)',mddata) # on rempli la section d'après (moyens s'il y en a, motiv s'il n'y en a pas) jus'qu'au début de la desction dispositif commençant par 'par ces motifs' ou 'en conséquences'
+    print(len(var))
+    if len(var)==3:
+        if moyens == '':
+            motiv, exp_reg , txt_suite = var
+            motiv = motiv 
+            mddata = exp_reg + txt_suite 
+        else:
+            moyens, exp_reg , txt_suite = var
+            moyens = moyens 
+            mddata = exp_reg + txt_suite 
+    else:
+        motiv = ''
 
-    xml = list()
+    
+    
+    dispo = mddata          # la partie dispositif correspond à ce qui reste
+
+    xml = list() # on rempli iterativement le fichier xml
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    xml.append('<Entête>')
+    xml.append(entete)
+    xml.append('</Entête>')
+    xml.append('<Exposé du litige>')
+    xml.append(expose)
+    xml.append('</Exposé du litige>')
+    xml.append('<Motivation>')
+    xml.append(motiv)
+    xml.append('</Motivation>')
+    xml.append('<Moyens>')
+    xml.append(moyens)
+    xml.append('</Moyens>')
+    xml.append('<Dispositif>')
+    xml.append(dispo)
+    xml.append('</Dispositif>')
     xml.append('</decision>')
-
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -4,6 +4,15 @@ Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
 import argparse
 import re
 
+def rewrite(txt_ini):
+    txt_res = re.sub(r'\n__+','',txt_ini) #enleve les lignes de _
+    txt_res = re.sub(r'\s*Page \d+ / \d+\s*','',txt_res) # enlève les mentions de page
+    txt_res = re.sub(r'Pourvoi .* 2023','',txt_res) # enleve le titre du pourvoi
+    txt_res = re.sub(r'([\.,\w\[\]])\n([a-zA-Z\[\]]|\d{1,2}(?!\.))',r'\1 \2',txt_res) # enleve les retours à la lignes qui coupent les phrases, on prend toujours sauf si la ligne en dessous ressemble au point d'une liste numérotée
+    txt_res = re.sub(r'([\.,\w\[\]])\n\n([a-z])',r'\1 \2',txt_res) # pour les phrases sur séparées par 2 retour lignes, on regarde su une ligne commence par une minuscule
+    txt_res = re.sub(r'\n{3,}','\n\n',txt_res) # on supprime tous les dauts de plus de 2 lignes
+    return txt_res
+
 
 def main():
     parser = argparse.ArgumentParser("text2md")
@@ -13,10 +22,10 @@ def main():
 
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
-
+    
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    mddata = rewrite(textdata)
 
     md = list()
     md.append("# Pourvoi NUM_POURVOI du DATE")
@@ -26,3 +35,5 @@ def main():
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+main()
