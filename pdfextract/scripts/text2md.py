2c3d43909ff4a559d79054e6213b3fbf72987cc5
Juan <juan-camilo.urbano_gomez@etu.minesparis.psl.eu>
2024-01-02 23:53:23 +0100

    improves text2md

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -14,15 +14,20 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    # Use regular expressions to remove unnecessary text
+    patternEndPage = re.compile(r'\s*Page \d+ \/ \d+\s*\n')
+    patternStartPage = re.compile(r'\f\s*Pourvoi N°(.*)\n')
+    pourvoiInfo = re.search(r'[nN]° [A-Z] (\d{2}-\d{2}.\d{3})\s?\w?', textdata)
+    date = re.search(r'DU (\d+ \w+ \d{4})', textdata)
+    mddata = re.sub(patternEndPage, '', textdata)
+    mddata = re.sub(patternStartPage, ' ', mddata)
+    mddata = re.sub(r'([,\w])\n[^\n]', r'\1 ', mddata) #join lines that are not a different paragraph
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f'# Pourvoi {pourvoiInfo.group(1)} du {date.group(1)}')
     md.append("")  # Ligne vide
     md.append(mddata)
 
-    outdata = '\n'.join(md) 
+    outdata = '\n'.join(md)
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
