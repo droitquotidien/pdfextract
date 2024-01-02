4281035807b035f69a67dd8d7bf44b2d76a379c5
cwotawa <70854059+cwotawa@users.noreply.github.com>
2024-01-02 18:51:31 +0100

    Update md2xml.py

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,12 +17,39 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    xmldata = re.split('\n+', mddata)
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    for line in xmldata:
+        if re.match('^# Pourvoi', line) != None:
+            xml.append("<p> " + line + "</p>")
+            xml.append("")
+            xml.append('<div class="ENTETE">')
+        elif re.match('^Faits et ', line) != None:
+            xml.append('</div>')
+            xml.append('<div class = "EXPOSE DU LITIGE">')
+            xml.append("<p> " + line + "</p>")
+        elif re.match('^((R(é|e)ponse de la (c|C)our)|(1. Le moyen))', line) != None:
+            xml.append('</div>')
+            xml.append('<div class = "MOTIVATION">')
+            xml.append("<p> " + line + "</p>")
+        elif re.match('^Enonc(é|e) d((u)|(es)) moyen', line) != None:
+            xml.append('</div>')
+            xml.append('<div class = "MOYENS">')
+            xml.append("<p> " + line + "</p>")
+        elif re.match('^Examen d((u)|(es)) moyen', line) != None:
+            xml.append('</div>')
+            xml.append('<div class = "MOYENS">')
+            xml.append("<p> " + line + "</p>")
+        elif re.match('^((PAR CES MOTIFS)|(EN CONSÉQUENCE))', line) != None:
+            xml.append('</div>')
+            xml.append('<div class = "DISPOSITIF">')
+            xml.append("<p> " + line + "</p>")
+        else:
+            xml.append("<p> " + line + "</p>")
+    xml.append('</div>')
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
