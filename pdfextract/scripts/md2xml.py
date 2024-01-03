92aa1369fe2f01a7832409590a24567fda1c1da6
francoismaze <70951522+francoismaze@users.noreply.github.com>
2024-01-03 22:10:37 +0100

    TP1 François Mazé
    Les deux fichiers de code (md2xml et text2md) modifiés; ainsi que les fichiers md et xml générés

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,15 +17,110 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
+    
     xmldata = mddata
+
+    # It was assumed that the document should be divided in the exact same way as it is done on Judilibre:
+        # En-tête
+        # Exposé du litige
+        # Moyens
+        # For every moyen: Enoncé du moyen and Motivation
+        # Dispositif
+    # Some examples from Judilibre have been used to determine the right regexps to use to perform the division
+
+    # Pourvoi N°Z 21-24.923 is structurally different from the other texts, since it only consists of an En-tête section, a Motivation section and a Dispositif section (on Judilibre)
+    # The code has been adapted to make it work on this type of poorly structured pourvois as well as on fully-structured documents
+
+    def add_paragraphs(string, xml_file):
+        par_division = re.split(r'\n+', string)
+        for p in par_division:
+            if p == "":
+                continue
+            xml_file.append("<p>" + p + "</p>")
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+
+    # En-tête section
+    division_entete = re.split(r'(Faits*\set\sprocédures*\n+)', xmldata, maxsplit = 1, flags = re.IGNORECASE)
+    if len(division_entete) == 3: # Corresponds to fully-structured documents
+        full_structure = True
+        entete = division_entete[0]
+        follow = division_entete[1] + division_entete[2]
+        xml.append('<entete>')
+        add_paragraphs(entete, xml)
+        xml.append('</entete>')
+    else: # Corresponds to poorly-structured documents, with only En-tête, Motivation and Dispositif sections
+        full_structure = False
+        division_entete = re.split(r'(a rendu la présente décision\.)', xmldata, maxsplit = 1, flags = re.IGNORECASE)
+        entete = division_entete[0] + division_entete[1]
+        follow = division_entete[2]
+        xml.append('<entete>')
+        add_paragraphs(entete, xml)
+        xml.append('</entete>')
+        # Motivation and Dispositif section (for poorly-structured documents)
+        division_motivation = re.split(r'(En conséquence, la Cour)|(PAR CES MOTIFS)', follow, maxsplit = 1, flags = re.IGNORECASE)
+        motivation = division_motivation[0]
+        dispositif = (division_motivation[1] if division_motivation[2] is None else division_motivation[2]) + division_motivation[3]
+        xml.append('<motivation>')
+        add_paragraphs(motivation, xml)
+        xml.append('</motivation>')
+        xml.append('<dispositif>')
+        add_paragraphs(dispositif, xml)
+        xml.append('</dispositif>')
+    
+    # Exposé du litige
+    if full_structure:
+        division_expose = re.split(r'(Examen d\w+ moyens*\n+)', follow, maxsplit = 1, flags = re.IGNORECASE)
+        expose = division_expose[0]
+        follow = division_expose[1] + division_expose[2]
+        xml.append('<expose_litige>')
+        add_paragraphs(expose, xml)
+        xml.append('</expose_litige>')
+
+    # Moyens
+    if full_structure:
+        xml.append('<moyens>')
+        division_moyens = re.split(r'(Enoncé d\w+ moyens*)', follow, flags = re.IGNORECASE)
+        if len(division_moyens) == 1:
+            division_motivation = re.split(r'(Réponse de la Cour)', division_moyens[0], flags = re.IGNORECASE)
+            add_paragraphs(division_motivation[0], xml)
+            for i, part in enumerate(division_motivation[1:]):
+                if i%2 == 1 :
+                    add_paragraphs(part, xml)
+                    xml.append('</motivation>')
+                else:
+                    xml.append('<motivation>')
+                    add_paragraphs(part, xml)
+        else:
+            add_paragraphs(division_moyens[0], xml)
+            for i, part in enumerate(division_moyens[1:]):
+                if i%2 == 0:
+                    xml.append('<enonce_moyen>')
+                    add_paragraphs(part, xml)
+                else:
+                    division_dispositif = re.split(r'(En conséquence, la Cour)|(PAR CES MOTIFS)', part, flags = re.IGNORECASE)
+                    if len(division_dispositif) > 1:
+                        part = division_dispositif[0]
+                        follow = (division_dispositif[1] if division_dispositif[2] is None else division_dispositif[2]) + division_dispositif[3]
+                    division_motivation = re.split(r'(Réponse de la Cour)', part, flags = re.IGNORECASE)
+                    add_paragraphs(division_motivation[0], xml)
+                    xml.append('</enonce_moyen>')
+                    xml.append('<motivation>')
+                    add_paragraphs(division_motivation[1]+division_motivation[2], xml)
+                    xml.append('</motivation>')
+        xml.append('</moyens>')
+
+    # Dispositif
+    if full_structure:
+        xml.append('<dispositif>')
+        add_paragraphs(follow, xml)
+        xml.append('</dispositif>')
+
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
-        f.write(outdata) 
+        f.write(outdata)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -18,8 +18,61 @@ def main():
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
 
+    #Extract pourvoi date
+    pattern_date = re.compile(r"DU\s(\d{1,2}\s\w*\s\d{4})\n")
+    search_date = re.search(pattern_date, mddata)
+    if search_date:
+        date = search_date.group(1).lower()
+    else : 
+        raise Exception("No pourvoi date was found") 
+
+    #Extract pourvoi number
+    pattern_number = re.compile(r"N°\s([A-Z]\s\d{2}\-\d{2}.*)\n", re.IGNORECASE)
+    search_number = re.search(pattern_number, mddata)
+    if search_number:
+        number = search_number.group(1)
+    else : 
+        raise Exception("No pourvoi number was found") 
+
+    #Remove new page characters
+    pattern_new_page_char = re.compile(r"\f[\f]*")
+    mddata = re.sub(pattern_new_page_char, "", mddata)
+
+    #Remove page numbers
+    pattern_page_num = re.compile(r"page\s\d+\s/\s\d+\n", re.IGNORECASE)
+    mddata = re.sub(pattern_page_num, "", mddata)
+
+    #Remove headers
+    pattern_header = re.compile(r"Pourvoi\sN°\d+\-\d+.*\d{1,2}\s\w*\s\d{4}\n", re.IGNORECASE)
+    mddata = re.sub(pattern_header, "", mddata)
+
+    # Remove all single linebreaks
+    pattern_single_lb = re.compile(r'(?<!\n)\n(?!\n)')
+    mddata = re.sub(pattern_single_lb, ' ', mddata)
+    
+    # Replace redundant linebreaks (>=3)
+    pattern_redundant_lb = re.compile(r'\n{2,}')
+    mddata = re.sub(pattern_redundant_lb, '\n\n', mddata)
+    
+    # Remove unnecessary spaces
+    pattern_useless_spaces = re.compile(r' +')
+    mddata = re.sub(pattern_useless_spaces, ' ', mddata)
+    
+    # Remove spaces at the beginning of paragraphs
+    pattern_spaces_start_par = re.compile(r'(?<=\n) +')
+    mddata = re.sub(pattern_spaces_start_par, '', mddata)
+    
+    # Delete paragraphs breaks when the second paragraph does not start with a capital letter or a number
+    pattern_wrong_par = r'(\n\n)(?![A-Z]|(\d+\.)\s)'
+    mddata = re.sub(pattern_wrong_par, r' ', mddata)
+
+    #Remove dashes
+    pattern_dashes = re.compile(r"_(_)+")
+    mddata = re.sub(pattern_dashes, "", mddata)
+
+    #Add title with number and date 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {number} du {date}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
