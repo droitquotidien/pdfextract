f4c6abcdd3dbbd5347261c05bae201decb0c6591
Thibaut CAILLIEREZ <thibaut.caillierez@etu.minesparis.psl.eu>
2024-01-04 11:43:44 +0100

    v0

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -4,25 +4,91 @@ Transforme un document md transformé avec text2md en XML découpé avec le "zon
 import argparse
 import re
 import sys
+import xml.etree.ElementTree as ET
+
+section_in = "<section>"
+section_out = "</section>"
+subsection_in = "<subsection>"
+subsection_out = "</subsection>"
+subsubsection_in = "<subsubsection>"
+subsubsection_out = "</subsubsection>"
+text_in = "<text>"
+text_out = "</text>"
+
+
 
 def main():
     parser = argparse.ArgumentParser("Markdown to XML")
     parser.add_argument('in_file', help="Markdown file")
     parser.add_argument('out_file', help="XML file")
     args = parser.parse_args()
-
+    decision = list()
     with open(args.in_file, "r", encoding="utf-8") as f:
         mddata = f.read()
 
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    currently_in_section = False
+    currently_in_subsection = False
+    currently_in_subsubsection = False
+    currently_in_text = False
+
+    
+
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    
+    for line in mddata.split('\n'):
+        if line.startswith('# '):
+            if currently_in_subsubsection:
+                xml.append(subsubsection_out)
+            if currently_in_subsection:
+                xml.append(subsection_out)
+            if currently_in_section:
+                xml.append(section_out)
+
+            xml.append(section_in)
+            xml.append("<title>" + line[2:] + "</title>")
+
+            currently_in_subsection = False
+            currently_in_subsubsection = False
+            currently_in_section = True
+
+        elif line.startswith('## '):
+            if currently_in_subsubsection:
+                xml.append(subsubsection_out)
+            if currently_in_subsection:
+                xml.append(subsection_out)
+
+            xml.append(subsection_in)
+            xml.append("<title>" + line[3:] + "</title>")
+
+            currently_in_subsection = True
+            currently_in_subsubsection = False
+
+
+        elif line.startswith('### '):
+            if currently_in_subsubsection:
+                xml.append(subsubsection_out)
+
+            xml.append(subsubsection_in)
+            xml.append("<title>" + line[4:] + "</title>")
+
+            currently_in_subsubsection = True
+
+        else:
+            xml.append("<text>" + line + "</text>")
+
+    if currently_in_subsubsection:
+                xml.append(subsubsection_out)
+    if currently_in_subsection:
+        xml.append(subsection_out)
+    if currently_in_section:
+        xml.append(section_out)
+
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -4,6 +4,51 @@ Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
 import argparse
 import re
 
+pourvoi_pattern = r'(n|N)°\s([A-Z])\s(\d{2}-\d{2}.\d{3})\s([A-Z]{1,2}-[A-Z]{1,2})?'
+date__pattern = r'(du|DU)\s(\d{1,2}\s[a-zéûA-ZÉÛ]+\s\d{4})'
+
+pagination_pattern = r'\s+Page\s\d+\s\/\s\d+'
+entete_pattern = r'\s+Pourvoi\s(n|N)°\s?[A-Z]?\s?(\d{2}-\d{2}.\d{3})\s?([A-Z]{1,2}-[A-Z]{1,2})?[a-zA-Z\s-]+(\d{1,2}\s[a-zéûA-ZÉÛ]+\s\d{4})'
+fin_page = r'\x0c'
+
+fait_et_procedure = r'Faits\set\sprocédure'
+examen_des_moyens = r'Examen\sdes\smoyens'
+conclusion = r'[\s\wéÉè]*,\sla\sCour\s:'
+sur_le_moyen = r'(S|s)ur\sle\s\w+\smoyen'
+enonce_du_moyen = r'Enoncé\sdu\smoyen'
+reponse_de_la_cour = r'Réponse\sde\sla\sCour'
+
+def find_title(textdata):
+    # lettre_prepourvoi = re.search(pourvoi_pattern, mddata).group(2)
+    num_pourvoi = re.search(pourvoi_pattern, textdata).group(3)
+    # lettre_postpourvoi = re.search(pourvoi_pattern, mddata).group(4)
+    date_pourvoi = re.search(date__pattern, textdata).group(2)
+    return f'Pourvoi {num_pourvoi} du {date_pourvoi}'
+
+def filter_data(textdata):
+    return re.sub(fin_page, '', re.sub(entete_pattern, '', re.sub(pagination_pattern, '', textdata)))
+
+def attribute_prefix(line: str) -> str :
+    if re.search(fait_et_procedure, line) or re.match(examen_des_moyens, line) or re.search(conclusion, line):
+        return '# '
+    elif re.search(sur_le_moyen, line):
+        return '## '
+    elif re.search(enonce_du_moyen, line) or re.search(reponse_de_la_cour, line):
+        return '### '
+    else : 
+        return ''
+    
+def generate_md(textdata):
+    prefix = ''
+    mddata = ""
+    for line in textdata.split('\n') :
+        if len(line) > 0 :
+            if line[0].islower() : 
+                print(line)
+                mddata += " " + line
+            else : 
+                prefix = attribute_prefix(line)
+                mddata += "\n" + prefix + line
 
 def main():
     parser = argparse.ArgumentParser("text2md")
@@ -14,13 +59,12 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    textdata_filtered = filter_data(textdata)
+
+    mddata = generate_md(textdata_filtered)
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
-    md.append("")  # Ligne vide
+    md.append(find_title(textdata_filtered))
     md.append(mddata)
 
     outdata = '\n'.join(md) 
