a6916c7512ce7a1f8a09b4783ba23498255dfd21
sgobbi <salome.gobbi@gmail.com>
2024-01-05 16:45:19 +0100

    fichiers python modifies

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -18,9 +18,42 @@ def main():
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
 
+    
+    # Renommer le md avec le numéro et la date du pourvoi
+    NUM = re.search('(?<=Pourvoi N°)([0-9]|-)+.[0-9]+',mddata).group(0)
+    DATE = re.search('(?<=DU )[0-9]+ [a-zA-ZéÉèÈàÀûÛ]+ [0-9]+',mddata).group(0)
+    titre= "# Pourvoi " + NUM + " du " + DATE
+       
+
+    #Enlever les numérotations de page
+    mddata = re.sub(r'\s*Page \d+ / \d+\s*','',mddata) 
+
+    #Enleve le numero de pourvoi
+
+    reg_expression_numero = re.compile(r'Pourvoi n° Z \d+-\d+\.\d+')
+    mddata = reg_expression_numero.sub('', mddata)
+
+    #Enlever les barres horizontales
+    reg_expression_barre = re.compile(r'_{2,}')
+    mddata = reg_expression_barre.sub('', mddata)
+
+    #Enlever les sauts de lignes de plus d'une ligne
+    reg_expression_sauts = re.compile(r"\n(\n)+", re.IGNORECASE)
+    mddata = re.sub(reg_expression_sauts, r"\n\n", mddata)   
+
+    #Enelver les sauts de lignes dans les paragraphes
+    reg_expression_paragraphe = re.compile(r'\n+\s*([^\nA-Z])')
+    textdata = reg_expression_paragraphe.sub(' \\1', textdata)
+
+    #Enlever les linebreaks qui coupent une phrase en 2
+    reg_expression_break = re.compile(r'\n(?![A-Z0-9])')
+    mddata = re.sub(reg_expression_break, '', mddata)
+
+
+     # Ligne vide
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
-    md.append("")  # Ligne vide
+    md.append("") 
+    md.append(titre) 
     md.append(mddata)
 
     outdata = '\n'.join(md) 
