86ff4c4c9dcf9fe76daac8f62d86f0c0e9b3b561
XiaohuLiu <40679102+peter12398@users.noreply.github.com>
2024-01-05 16:18:09 +0100

    Update md2xml.py

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,15 +17,61 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
+    
+    """
+    Les zones sont notamment:
+    Entête
+    Exposé du litige
+    Motivation
+    Moyens
+    Dispositif
+    """
+    # Entête
+    
+    def capture_content(pattern = r'^(.*?)(?=RÉPUBLIQUEFRANÇAISE)', mddata = mddata):
+        matches = re.search(pattern, mddata, re.DOTALL | re.IGNORECASE)
+        if matches:
+            captured_content = matches.group(1)
+            print(captured_content)
+        else:
+            captured_content = ''
+            print("Pattern not found.")
+        return captured_content
+        
+    Entete = capture_content(pattern = r'^(.*?)(?=RÉPUBLIQUEFRANÇAISE)', mddata = mddata)
+    Expose_du_litige = capture_content(pattern = r'RÉPUBLIQUEFRANÇAISE\n(.*?)(?=\n(?:Sur le rapport|$))', mddata = mddata)
+    Motivation = capture_content(pattern = r'Sur le rapport\n(.*?)(?=\n(?:Faits et procédure|Examen des moyens$))', mddata = mddata)
+    Moyens = capture_content(pattern = r'Faits et procédure|Examen des moyens\n(.*?)(?=\n(?:PAR CES MOTIFS|EN CONSÉQUENCE, la Cour :|$))', mddata = mddata)
+    Dispositif = capture_content(pattern = r'(?:PAR CES MOTIFS|EN CONSÉQUENCE, la Cour :)\s*(.*)', mddata = mddata)
+
     xmldata = mddata
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
-    xml.append('<decision>')
-    xml.append(xmldata)
-    xml.append('</decision>')
+    xml.append('<Entête>')
+    xml.append(Entete)
+    xml.append('</Entête>')
+    
+    xml.append('<Expose_du_litige>')
+    xml.append(Expose_du_litige)
+    xml.append('</Expose_du_litige>')
+
+    xml.append('<Motivation>')
+    xml.append(Motivation)
+    xml.append('</Motivation>')
+
+    xml.append('<Moyens>')
+    xml.append(Moyens)
+    xml.append('</Moyens>')
+
+    xml.append('<Dispositif>')
+    xml.append(Dispositif)
+    xml.append('</Dispositif>')
 
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata) 
+
+if __name__ == "__main__":
+    main()
