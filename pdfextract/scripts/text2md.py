55eef84aa04e55cdb6a9341edabdb191d667e591
XiaohuLiu <40679102+peter12398@users.noreply.github.com>
2024-01-05 16:17:45 +0100

    Update text2md.py

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -16,13 +16,38 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    
+    # Define the pattern to match
+    #pattern = r'Page \d+ / \d+'
+    pattern = r'\s*Page\s*\d+\s*/\s*\d+\s*'
+    pattern2 = r'\s*Pourvoi\s*(N°\d+-.*?)-.*?\s*(\d+\s*.*?\s*\d+)'
+                                          
+    
+    # Use re.sub() to replace the matched pattern with an empty string
+    tmp = re.sub(pattern, '', textdata)
+    
+    [(NUM_POURVOI, DATE)] = re.findall(pattern2, tmp)
+    tmp = re.sub(pattern2, '', tmp)
+    
+    # supprimer les retours à la ligne
+    # Remplacer les sauts de ligne au milieu des paragraphes par un espace
+    tmp = re.sub(r'(?<!\n)\n(?!\n)', ' ', tmp)
+
+    # Remplacer les sauts de ligne entre deux paragraphes par une seule ligne vide
+    result = re.sub(r'\n\n+', '\n\n', tmp)
+    
+    
+    
+    mddata = result
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append("# Pourvoi {} du {}".format(NUM_POURVOI,DATE))
     md.append("")  # Ligne vide
     md.append(mddata)
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+if __name__ == "__main__":
+    main()
