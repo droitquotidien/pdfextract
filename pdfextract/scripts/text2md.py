8d0ca9506efa92a480e5009a013dd1f4ddc6aafb
Your Name <you@example.com>
2024-01-06 07:09:26 +0100

    first algorithm completed

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -13,16 +13,28 @@ def main():
 
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
+    #lines = re.split('\n+', textdata)
+    #print(textdata.split()[:50])
+    date = re.search('([0-3]\d)\s[A-Za-zÀ-ÖØ-öø-ÿ]{4,12}\s(20[0-2]\d)', textdata, re.MULTILINE).group()
+    pourvoi = re.search('\d{2}-\d{2}.\d{3}', textdata, re.MULTILINE).group()
+    textdata = re.sub('\n{2,}\s{1,}Page\s\d+\s\/\s\d+|(\f).{1,}\n', '\n', textdata)
+    textdata = re.sub('\n{3,}|(?<=[;:!?.])\s(?=[A-Z])', '\n\n', textdata)
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
+    index = textdata.find("DU " + date.upper())
+    index_split = index + len(date) + 3
+    beginning = textdata[:index_split]
+    end = re.sub('(?<![;:!?.])\n\n|(?<=M\.)\n\n|(?<=\d\.)\n\n', ' ', textdata[index_split:]) 
+    textdata = beginning + '\n\n' + end
+
     mddata = textdata
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {pourvoi} du {date}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+main()
