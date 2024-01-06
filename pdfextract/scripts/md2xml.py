1c9949a252b85f882d0189f4127e3e0c0eef9576
MaxenceMo <72147977+MaxenceMo@users.noreply.github.com>
2024-01-06 17:48:34 +0100

    Ready to merge

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -14,18 +14,53 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         mddata = f.read()
 
-    # Transform mddata with re here
-    # see https://docs.python.org/fr/3/library/re.html
-    # document must be a valid XML file
-    xmldata = mddata
+    xmldataSplit = re.split('\n+', mddata)
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
-    xml.append('<decision>')
-    xml.append(xmldata)
-    xml.append('</decision>')
+    xml.append('<article>')
+    xml.append('    <div class="decision-content">')
+
+    for line in xmldataSplit:
+        if re.search('^# Pourvoi', line):
+            xml.append("        <h1> " + line + "</h1>")
+            xml.append('        <div class="decision-element">')
+            xml.append('            <h2> Texte de la décision </h2>')
+            xml.append('            <div class="decision-accordeon">')
+            xml.append('                <h3 >Entête </h3>')
+        elif re.search("^faits et procédure", line, re.IGNORECASE):
+            xml.append('            </div>')
+            xml.append('            <div class="decision-accordeon">')
+            xml.append('                <h3> Exposé du litige </h3>')
+            xml.append("                <p> " + line + "</p>")
+        elif re.search('^(Enonc(é|e)|Examen) d((u)|(es)) moyen(|s)', line, re.IGNORECASE):
+            xml.append('            </div>')
+            xml.append('            <div class="decision-accordeon">')
+            xml.append('                <h3> Moyens </h3>')
+            xml.append("                <p> " + line + "</p>")
+        elif re.search('^R(é|e)ponse de la cour', line, re.IGNORECASE):
+            xml.append('            </div>')
+            xml.append('            <div class="decision-accordeon">')
+            xml.append('                <h3> Motivation </h3>')
+            xml.append("                <p> " + line + "</p>")
+        elif re.search('^((par ce(|s) motif(|s))|(en conséquence))', line, re.IGNORECASE):
+            xml.append('            </div>')
+            xml.append('            <div class="decision-accordeon">')
+            xml.append('                <h3> Dispositif </h3>')
+            xml.append("                <p> " + line + "</p>")
+        else:
+            xml.append("                <p> " + line + "</p>")
+
+    xml.append('            </div>')
+    xml.append('        </div>')
+
+    xml.append('    </div>')
+    xml.append('</article>')
 
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata) 
+
+
+main()
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -14,15 +14,39 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
-    md.append("")  # Ligne vide
+    POURVOI_NUM = re.search('(?<=Pourvoi N°)\d+-\d+\.\d+',mddata, re.MULTILINE).group()
+    DATE = re.search('[0-9]{1,2} \w+ [0-9]{4}',mddata, re.MULTILINE).group()
+    TITLE = "# Pourvoi " + POURVOI_NUM + " du " + DATE
+    md.append(TITLE)
+    md.append("")
+
+    #Enlever les numérotations
+    mddata = re.sub(r'\s*Page \d+ / \d+\s*','',mddata)
+
+    #Enlever le pourvoi
+    mddata = re.sub('Pourvoi N°\d+-\d+\.\d+(-[a-zA-ZéÉèÈàÀûÛ\s]+)?(?:\s{0,2}|$)', '',mddata)
+
+    #Enlever la date
+    mddata = re.sub('\s{2}[0-9]{1,2} \w+ [0-9]{4}', '',mddata)
+
+    #Enlever les barres horizontales
+    mddata = re.sub(r'_{2,}', '', mddata)
+
+    #Enlever les sauts de lignes de plus d'une ligne
+    mddata = re.sub(r"\n(\n)+", '\n\n', mddata)
+
+    #Enelver les sauts de lignes et linebreaks dans les paragraphes
+    reg_expression_paragraphe = re.compile(r'\n+\s*([^\nA-Z0-9])')
+    mddata = reg_expression_paragraphe.sub(' \\1', mddata)
+
     md.append(mddata)
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+
+main()
