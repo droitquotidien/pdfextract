a6e5027959d8633bda29245905291c8f3fa1fb6a
Your Name <you@example.com>
2024-01-06 09:37:16 +0100

    all fixed

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -19,10 +19,53 @@ def main():
     # document must be a valid XML file
     xmldata = mddata
 
+    en_tete = re.compile('a\srendu\sle\sprésent\sarrêt\.', re.IGNORECASE)
+    expose = re.compile("faits\set\sprocédure", re.IGNORECASE)
+    moyens = re.compile("(examen\s(du|des)\smoyen(\n|s\n))|(enoncé\s(du|des)\smoyen(|s))", re.IGNORECASE)
+    motivations = re.compile("réponse\sde\sla\scour\n", re.IGNORECASE)
+    dispositifs = re.compile("par\s(ce|ces)\smotif(|s)", re.IGNORECASE)
+
+    ind_en_tete = next(re.finditer(en_tete, xmldata)).start(0)
+    ind_expose = next(re.finditer(expose, xmldata)).start(0)
+    ind_dispositifs = next(re.finditer(dispositifs, xmldata)).start(0)
+
+    ind_moyens, ind_motivations = [], []
+    for i in  re.finditer(moyens, xmldata):
+        ind_moyens.append(i.start(0))
+    ind_moyens.remove(ind_moyens[1])
+    for i in  re.finditer(motivations, xmldata):
+        ind_motivations.append(i.start(0))
+
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    
+    xml.append('<div class="EN TETE">')
+    xml.append(xmldata[:ind_en_tete])
+    xml.append('<\div>')
+
+    xml.append('<div class="EXPOSE">')
+    if ind_expose:
+        xml.append(xmldata[ind_expose:ind_moyens[0]])
+    xml.append('<\div>')
+    
+    #there might be more than 1 moyen/dispositif
+    for i in range(len(ind_motivations)):
+        xml.append(f'<div class="MOYENS {i}">')
+        xml.append(xmldata[ind_moyens[i]:ind_motivations[i]])
+        xml.append('<\div>')
+
+        xml.append(f'<div class="MOTIVATIONS {i}">')
+        if i < len(ind_motivations) - 1:
+            xml.append(xmldata[ind_motivations[i]:ind_moyens[i+1]])
+        else:
+            xml.append(xmldata[ind_motivations[i]:ind_dispositifs])
+        xml.append('<\div>')
+
+    xml.append('<div class="DISPOSITIFS">')
+    xml.append(xmldata[ind_dispositifs:])
+    xml.append('<\div>')
+
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -13,20 +13,28 @@ def main():
 
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
-    #lines = re.split('\n+', textdata)
-    #print(textdata.split()[:50])
-    date = re.search('([0-3]\d)\s[A-Za-zÀ-ÖØ-öø-ÿ]{4,12}\s(20[0-2]\d)', textdata, re.MULTILINE).group()
-    pourvoi = re.search('\d{2}-\d{2}.\d{3}', textdata, re.MULTILINE).group()
-    textdata = re.sub('\n{2,}\s{1,}Page\s\d+\s\/\s\d+|(\f).{1,}\n', '\n', textdata)
-    textdata = re.sub('\n{3,}|(?<=[;:!?.])\s(?=[A-Z])', '\n\n', textdata)
-
-    index = textdata.find("DU " + date.upper())
-    index_split = index + len(date) + 3
-    beginning = textdata[:index_split]
-    end = re.sub('(?<![;:!?.])\n\n|(?<=M\.)\n\n|(?<=\d\.)\n\n', ' ', textdata[index_split:]) 
-    textdata = beginning + '\n\n' + end
 
     mddata = textdata
+
+    #finding date and pourvoi number
+    date = re.search('([0-3]\d)\s[A-Za-zÀ-ÖØ-öø-ÿ]{4,12}\s(20[0-2]\d)', mddata, re.MULTILINE).group()
+    pourvoi = re.search('\d{2}-\d{2}.\d{3}', mddata, re.MULTILINE).group()
+    
+    #removing pages number
+    mddata = re.sub('\n{1,}\s{1,}Page\s\d+\s\/\s\d+|(\f).{1,}|(\f){1,}', ' ', mddata)
+   
+   #removing unnecessary lines between paragraphs
+    mddata = re.sub('\n{3,}|(?<=(!M|!\d)[;:!?.])\s', '\n\n', mddata)
+
+    index = mddata.find("DU " + date.upper())
+    index_split = index + len(date) + 3
+    beginning = mddata[:index_split]
+
+    find_linebreaks = re.compile("([^\n\s])\n([^\n\s])")
+    def repair(matchobj):
+        return matchobj.group(1) + " " + matchobj.group(2)
+    end = re.sub(find_linebreaks, repair, mddata[index_split:])
+    mddata = beginning + '\n\n' + end
 
     md = list()
     md.append(f"# Pourvoi {pourvoi} du {date}")
