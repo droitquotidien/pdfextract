ef9e57753724dd4a897fefe8ccce483732608814
unknown <thibaut.millot@etu.minesparis.psl.eu>
2024-01-06 23:22:36 +0100

    md2xml.py and text2md.py are modified to work with the given exemples.

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -18,7 +18,41 @@ def main():
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
     xmldata = mddata
+    last_flag = None
+    xmldata = "<Entête>\n"+xmldata
+    last_flag = "</Entête>"
 
+
+    expose = re.compile(r"faits et procédure", re.IGNORECASE)
+    examen = re.compile(r"examen (du|des) moyens*", re.IGNORECASE)
+    moyens = re.compile(r"enoncé (du|des) moyens*", re.IGNORECASE)
+    motivation = re.compile(r"réponse de la cour", re.IGNORECASE)
+    dispositif = re.compile(r"((par ces motifs,)|(en conséquence,))", re.IGNORECASE)
+
+    # Place XML tags where there are the corresponding match if there is a match
+    if re.search(expose, xmldata):
+        xmldata = re.sub(expose, "\n{}\n<Exposé>\n".format(last_flag), xmldata)
+        last_flag = "</Exposé>"
+    elif re.search(re.compile(r"a rendu (le présent arrêt|la présente décision)", re.IGNORECASE), xmldata):
+        Match = re.search(re.compile(r"a rendu (le présent arrêt|la présente décision).*\n", re.IGNORECASE), xmldata)
+        xmldata = re.sub(re.compile(r"a rendu (le présent arrêt|la présente décision).*\n", re.IGNORECASE), "{}\n{}\n<Exposé>\n".format(Match[0],last_flag), xmldata)
+        last_flag = "</Exposé>"
+    if re.search(examen, xmldata):
+        xmldata = re.sub(examen, "\n{}\n<Examen>\n".format(last_flag), xmldata)
+        last_flag = "</Examen>"
+    if re.search(moyens, xmldata):
+        xmldata = re.sub(moyens, "\n{}\n<Moyens>\n".format(last_flag), xmldata)
+        last_flag = "</Moyens>"
+    if re.search(motivation, xmldata):
+        xmldata = re.sub(motivation, "\n{}\n<Motivation>\n".format(last_flag), xmldata)
+        last_flag = "</Motivation>"
+    if re.search(dispositif, xmldata):
+        dispositif_not_to_replace = re.compile(r"\n((par ces motifs,)|(en conséquence,)).*\n", re.IGNORECASE)
+        Match = re.search(dispositif_not_to_replace, xmldata)
+        xmldata = re.sub(dispositif_not_to_replace, "\n{}\n<Dispositif>\n{}".format(last_flag, Match[0]), xmldata)
+        last_flag = "</Dispositif>"
+    # Add XML tags at the end of the document
+    xmldata = xmldata + "\n{}\n".format(last_flag)
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
@@ -29,3 +63,6 @@ def main():
 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata) 
+
+if __name__ == '__main__':
+    main()
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -17,12 +17,42 @@ def main():
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
+    # Get date and pourvoi number
+    pattern_date = r"(\d+\s+\D+\s+\d{4})"
+    pattern_pourvoi = r"n°\s+(\D+\s+\d{2}\-\d{2}.*)\n"
+    DATE = re.findall(pattern_date, mddata)[0]
+    NUM_POURVOI = re.findall(pattern_pourvoi, mddata)[0]
+    
+    # Delete lines composed with _ only
+    mddata = re.sub(r"^_+$", r"", mddata, flags=re.MULTILINE)
+    
+    # Remove footers
+    mddata = re.sub(r"\n\s*Page\s*\d+\s*/\s*\d+\n*\s*", r"\n", mddata)
+    # Remove headers
+    mddata = re.sub("\n\s*Pourvoi\s*N°.*{}\n".format(DATE), r"\n", mddata)
+    
+    # Remove empty lines at the beginning of the document (if there are any)
+    mddata = re.sub(r"^\s*\n", r"", mddata)
+    
+    # Keep only one blank line between paragraphs
+    mddata = re.sub(r"\n\s*\n", r"\n\n", mddata)
+
+    # Remove return to line in paragraphs but keep them at the end of paragraphs
+    mddata = re.sub(r"(?<!\n)\n(?!\n)", r" ", mddata)
+
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append("# Pourvoi {} du {}".format(NUM_POURVOI, DATE))
     md.append("")  # Ligne vide
+
+
+
     md.append(mddata)
+
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+if __name__ == '__main__':
+    main() 
