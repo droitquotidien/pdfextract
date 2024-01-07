e6d6d4ccee729d3b1ddf7518c957c81e510127df
Emeric Marès <emeric.mares@mines-paristech.fr>
2024-01-07 19:30:49 +0100

    text to markdown

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -18,10 +18,47 @@ def main():
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
 
+    # extractin the pourvoi number
+    pattern_for_number = r'Pourvoi N°(\d+-\d+\.\d+)'
+    match_number = re.search(pattern_for_number, textdata)
+    if match_number:
+        NUM_POURVOI = match_number.group(1)
+        print("Extracted number:", NUM_POURVOI)
+    else:
+        NUM_POURVOI = ""
+        print("Number not found.")
+
+    # extracting the date
+    pattern_for_date = r'Pourvoi N°\d+-\d+\.\d+.*?(\d{1,2} \w+ \d{4})'
+    match_date = re.search(pattern_for_date, textdata)
+    if match_date:
+        DATE = match_date.group(1)
+        print("Extracted date:", DATE)
+    else:
+        DATE = ""
+        print("Date not found.")
+
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {NUM_POURVOI} du {DATE}")
     md.append("")  # Ligne vide
-    md.append(mddata)
+
+    # deleting the page number and header
+    mddata = re.sub(r'\n.*Page \d+ / \d+\n', '', mddata)
+    mddata = re.sub(r'.*Pourvoi N°.*\n', '', mddata)
+
+    # reconstruire des paragraphes continus
+    mddata = re.sub(r'(?<!\.)\n+(?=[a-z])', '\n', mddata)
+
+    # Supprimer tous les retours chariot dans un paragraphe
+    mddata = re.sub(r'(.)\n([^_\n])', r'\1 \2', mddata)
+
+    # Ne garder qu’une seule ligne vide entre deux paragraphes
+    mddata = re.sub(r'\n{3,}', '\n\n', mddata)
+
+    # enlever les derniers returns
+    mddata = re.sub(r'\n+$', '', mddata)
+
+    md.append(mddata[:-1])
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
