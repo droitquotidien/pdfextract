340b0360f662e8d9bfdb83f01e9f53a9b779bf51
TasnimKT <tasnimkazitani@gmail.com>
2024-01-07 13:14:48 +0100

    TP1 Tasnim Kazi Tani

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -19,13 +19,62 @@ def main():
     # document must be a valid XML file
     xmldata = mddata
 
-    xml = list()
-    xml.append('<?xml version="1.0" encoding="utf-8"?>')
-    xml.append('<decision>')
-    xml.append(xmldata)
-    xml.append('</decision>')
+    #On découpe notre texte en plusieurs parties
+    mots_cles = ["Faits et procédure", "Examen des moyens", "Réponse de la Cour", "PAR CES MOTIFS"]
+    pattern = re.compile('|'.join(map(re.escape, mots_cles)))
+    parties = re.split(pattern, xmldata)
+
+    print(len(parties))
+
+    #On verifie que ca fonctione
+    if len(parties) > 5:
+        print("if")
+        xml = list()
+        xml.append('<?xml version="1.0" encoding="utf-8"?>')
+        xml.append('<decision>')
+        xml.append('<Entête>')
+        xml.append('ENTETE')
+        xml.append('')
+        xml.append(parties[0])
+        xml.append('</Entête>')
+        xml.append('')
+        xml.append('<Exposé_du_litige>')
+        xml.append('Exposé du litige')
+        xml.append('')
+
+        xml.append(parties[1])
+        xml.append('</Exposé_du_litige>')
+        xml.append('')
+        xml.append('<Motivation>')
+        xml.append('Motivation')
+        xml.append('')
+        xml.append(parties[2])
+        xml.append('</Motivation>')
+        xml.append('')
+        xml.append('<Moyens>')
+        xml.append('Moyens')
+        xml.append('')
+        xml.append(parties[3])
+        xml.append('</Moyens>')
+        xml.append('')
+        xml.append('<Dispositif>')
+        xml.append('Dispositifs')
+        xml.append('')
+        xml.append(parties[4])
+        xml.append('</Dispositif>')
+        xml.append('')
+        xml.append('</decision>')
+
+    else:
+        print("else")
+        xml = list()
+        xml.append('<?xml version="1.0" encoding="utf-8"?>')
+        xml.append('<decision>')
+        xml.append(xmldata)
+        xml.append('</decision>')
+
 
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
-        f.write(outdata) 
+        f.write(outdata)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -14,15 +14,43 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
+        # Supprimer les sauts de page
+        clean_data = re.sub(r'\n', '', textdata)
+
+        #supprimer les ___
+        clean_data = re.sub(r'_+','',clean_data)
+
+        #supprimer les pages
+        clean_data = re.sub(r'\s*Page \d+ / \d+\s*','',clean_data)
+
+        #Remplacer les sauts de lignes par des virgules au sein d'un paragraphe
+        clean_data = re.sub(r'\n', ', ', clean_data)
+
+        #Remplacer les sauts de paragraphe par des sauts de ligne simple
+        clean_data = re.sub(r'(\r\n|\n\n)', '\n', clean_data)
+
+        #Supprimer les retours chariots au milieu d'une phrase
+        clean_data = re.sub(r'([\.,\w\[\]])\n([a-zA-Z\[\]]|\d{1,2}(?!\.))',r'\1 \2',clean_data)
+
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    mddata = clean_data
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+
+    #Ajouter un tire en haut du doc en markdown
+    NUM_POURVOI = re.search('(?<=Pourvoi N°)([0-9]|-)+.[0-9]+',mddata).group(0)
+    DATE = re.search('(?<=DU )[0-9]+ [a-zA-ZéÉèÈàÀûÛ]+ [0-9]+',mddata).group(0)
+    title = "# Pourvoi " + NUM_POURVOI + " du " + DATE
+    md.append(title)
     md.append("")  # Ligne vide
     md.append(mddata)
+
+
+    md.append("")
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+
+
