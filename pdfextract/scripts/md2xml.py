b8aad24fb30498eaac54a6ed225116d2d6bad10b
Marie HV <m.a.r.humbletvertongen@student.tudelft.nl>
2024-01-07 21:12:37 +0100

    .py file modif

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -5,6 +5,13 @@ import argparse
 import re
 import sys
 
+def process_section(regex, mddata, default=''):
+    var = re.split(regex, mddata)
+    if len(var) == 3:
+        return var[0] + var[1], var[2] #
+    else:
+        return default, mddata
+
 def main():
     parser = argparse.ArgumentParser("Markdown to XML")
     parser.add_argument('in_file', help="Markdown file")
@@ -17,12 +24,27 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    # xmldata = mddata
+    
+    # Splitting and processing sections
+    entete, mddata = process_section(r'(DU \d+ \w* \d{4})', mddata)
+    expose, mddata = process_section(r'(Sur le rapport)', mddata)
+    motivation, mddata = process_section(r'(Examen d.*moyens?)', mddata)
+    moyens, mddata = process_section(r'(PAR CES MOTIFS.*la Cour :|EN CONSÉQUENCE.*la Cour :)', mddata, None)
+    dispositif = mddata
+
+    # Setting dispositif section
+    dispositif = mddata
+
+    # Creating xml data
+    xmldata = [f'<Entête>{entete}</Entête>', f'<Exposé_du_litige>{expose}</Exposé_du_litige>',
+            f'<Motivation>{motivation}</Motivation>', f'<Moyens>{moyens}</Moyens>',
+            f'<Dispositif>{dispositif}</Dispositif>']
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    xml.extend(xmldata)
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -1,9 +1,26 @@
 """
-Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
+Transforme un document texte issu d'un PDF de la cour de cassation en Markdown.
 """
 import argparse
 import re
 
+def processed_text(txt):
+    # Remove page and number mentions
+    txt = re.sub(r'\s*Page \d+ / \d+\s*', '', txt)
+
+    # Remove the title of the appeal that contains "Pourvoi" and ends with a year
+    txt = re.sub(r'Pourvoi .* \d{4}', '', txt)
+
+    # Remove lines with underscores
+    txt = re.sub(r'\n__+', '', txt)
+
+    # Remove consecutive line breaks (more than 2)
+    txt = re.sub(r'\n{3,}', '\n\n', txt)
+
+    # Remove cutted sentences
+    txt = re.sub(r'([a-z].*?)\n([^\n])', '\\1', txt)
+
+    return txt
 
 def main():
     parser = argparse.ArgumentParser("text2md")
@@ -16,10 +33,16 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
+        
+    # Rename
+    NUM_POURVOI = re.search('(?<=Pourvoi N°)([0-9]|-)+.[0-9]+',mddata).group(0)
+    DATE = re.search('(?<=DU )[0-9]+ [a-zA-ZéÉèÈàÀûÛ]+ [0-9]+',mddata).group(0)
+
     mddata = textdata
+    mddata = processed_text(mddata)
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append("# Pourvoi " + NUM_POURVOI + " du " + DATE)
     md.append("")  # Ligne vide
     md.append(mddata)
 
