850df6ee4a662418d4826b840e26c38d4f976392
jeremy2056 <73949947+jeremy2056@users.noreply.github.com>
2024-01-07 19:38:56 +0100

    Update text2md.py
    creation de la transformation du texte en md

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -16,10 +16,37 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
+    #on garde le titre
     mddata = textdata
+    pattern = r"\d{1,2}\s\w+\s\d{4}"
+    ici = re.search(pattern,textdata)
+    second_pattern = r"N°\d+"
+    la_bas =  re.search(second_pattern,textdata)
+    titre = "# Pourvoi " + la_bas.group(0)[2:] + " du " + ici.group(0)
+
+    #on retire l'en-tete
+
+    pattern_to_remove = r"Pourvoi([\s\S]*?)\n"
+    mddata = re.sub(pattern_to_remove,"",mddata)
+    pattern_to_remove = r"Page([\s\S]*?)\n"
+    mddata = re.sub(pattern_to_remove,"",mddata)
+
+    #retrait des sauts de ligne
+
+    pattern_to_remove = r"\\n\\s*\\n" "\\n"
+    mddata = re.sub(pattern_to_remove,"",mddata)
+
+    #retrait des retours à la ligne 
+    pattern_to_remove = r"\n"
+    mddata = re.sub(pattern_to_remove,"",mddata)
+
+    #rajout d'une ligne en fin de chaque paragraphe
+    pattern_to_change = r"."
+    mddata = re.sub(pattern_to_change,".\n\n",mddata)
+
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(titre)
     md.append("")  # Ligne vide
     md.append(mddata)
 
