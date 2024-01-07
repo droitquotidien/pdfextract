b5ceae48c81dac86a2d0a6eb019a4f8e68782e88
Michael Vincendon <mikvincendon@gmail.com>
2024-01-07 22:03:27 +0100

    Proposer des algos pour md2xml et text2md

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,7 +17,27 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    xmldata_items = list()
+    paragraphs = re.split(r'(.*)\n\n', mddata)
+    zones = {"Exposé du litige": r'Faits et procédure',
+           "Motivation": r'(Réponse de la Cour|Aux termes de l\'article|nature à entraîner la cassation)',
+           "Moyens": r'moyen',
+           "Dispositif": r'la Cour :'}
+    xmldata_items.append('<div class="Entête">')
+    zone_actuelle = "Entête"
+    for parag in paragraphs:
+        if parag != "":
+            for zone in zones.keys():
+                if zone != zone_actuelle and re.search(zones[zone], parag):
+                    xmldata_items.append('</div">')
+                    xmldata_items.append(f'<div class="{zone}">')
+                    zone_actuelle = zone
+                    break
+
+            xmldata_items.append('<p>' + parag + "</p>")
+
+    xmldata_items.append('</div>')
+    xmldata = '\n'.join(xmldata_items)
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -17,9 +17,16 @@ def main():
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
+    m = re.search(r".*Pourvoi N°(\d+-.+)-.*\s{2,}(.*)", mddata)
+    NUM_POURVOI, DATE = m.group(1), m.group(2)
+
+    mddata = re.sub(r".*Page .* / .*", r"\n", mddata)       # Remove "Page..."
+    mddata = re.sub(r".*Pourvoi N°(.*)", r"\n", mddata)     # Remove "Pourvoi..."
+    mddata = re.sub(r"\n+([^A-Z\d_])", r" \1", mddata)         # Remove non-new paragraph \n
+    mddata = re.sub(r"\n+", r"\n\n", mddata)                # Format paragraph separation
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {NUM_POURVOI} du {DATE}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
