8e54509991123795a3299d7867fe9bb1a386a104
Ishfaaaaaq <ishfaaqrumjaun28@gmail.com>
2024-01-07 23:55:53 +0400

    TP 1 Ishfaaq Rumjaun

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -19,10 +19,49 @@ def main():
     # document must be a valid XML file
     xmldata = mddata
 
+    ###Identification des patterns pour délimiter les zones (arbitrairement)
+
+    #Pour l'entête, on part du fait que nous avons défini cela comme une paragraphe donc tout les informations restant sont supprimés.
+    #En bref, l'entête présente les personnes présentent et donne une introduction. D'ou le choix de pattern.
+    entete_match = re.search(r'(^\s*DÉCISION)(.*?)(?=\s*(Faits et procédure|1\.))', mddata, re.DOTALL | re.MULTILINE)
+    entete_zone = entete_match.group(0) if entete_match else ''
+
+    # Exposé du litige : Exposé implique les fait et procédure ou bien les bullets points
+    expose_match = re.search(r'(\s*(Faits et procédure|1\.)(.*?)(?=\s*(Enoncé des moyens)))', mddata, re.DOTALL | re.MULTILINE)
+    expose_zone = expose_match.group(0) if expose_match else ''
+
+    # Moyens : c'est l'énoncé des moyens
+    moyens_match = re.search(r'(\s*(Enoncé des moyens)(.*?)((?=\s*Réponse de la Cour)))', mddata, re.DOTALL | re.MULTILINE)
+    moyens_zone = moyens_match.group(0) if moyens_match else ''
+
+    # Motivation : Ce qui a motivé la Cour pour sa décision d'où la reponse de la Cour
+    motivation_match = re.search(r'\s*(Réponse de la Cour)(.*?)(?=\s*(PAR CES MOTIFS|EN CONSÉQUENCE))', mddata, re.DOTALL | re.MULTILINE)
+    motivation_zone = motivation_match.group(0) if motivation_match else ''
+
+    # Dispositif : C'est les conclusions. 
+    dispositif_match = re.search(r'\s*(PAR CES MOTIFS|EN CONSÉQUENCE)([\s\S]*)$', mddata, re.DOTALL | re.MULTILINE)
+    dispositif_zone = dispositif_match.group(0) if dispositif_match else ''
+
+    #mise en place des formes de chaque zone
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
     xml.append('<decision>')
-    xml.append(xmldata)
+    #xml.append(xmldata)
+    xml.append('<div class = "entete">')
+    xml.append(entete_zone)
+    xml.append('<div>')
+    xml.append('<div class = "expose du litige">')
+    xml.append(expose_zone)
+    xml.append('<div>')
+    xml.append('<div class = "motivation">')
+    xml.append(motivation_zone)
+    xml.append('<div>')
+    xml.append('<div class = "moyens">')
+    xml.append(moyens_zone)
+    xml.append('<div>')
+    xml.append('<div class = "dispositif">')
+    xml.append(dispositif_zone)
+    xml.append('<div>')
     xml.append('</decision>')
 
     outdata = '\n'.join(xml)
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -18,8 +18,39 @@ def main():
     # see https://docs.python.org/fr/3/library/re.html
     mddata = textdata
 
+    ###Ajout du titre de la forme "Pourvoi NUM du DATE"
+
+    pattern_NUM = re.compile(r'n°\s[A-Z]\s(\d{2}\-\d{2}\.\d{3})', re.IGNORECASE) # On vérifie n° X 00-00.000 et on capture (00-00.000)
+    # On met IGNORECASE car dans certains documents, il peut y avoir upper ou lower cases
+    NUM = pattern_NUM.search(mddata)
+    pattern_date = re.compile(r'DU\s(\d{1,2}\s\S+\s\d{4})', re.IGNORECASE) #1 ou 2 chiffre pour la date, un espace, \S+ pour plusieurs caractère qui représentent le mois, un espace et puis l'année.
+    date = pattern_date.search(mddata)
+
+    ### Suppression du numéro de page : Pattern
+    page_number_pattern = re.compile(r'[\n].*\bpage\s\d+\s\/\s\d+.*[\n]*.*', re.IGNORECASE)
+    ### Suppression des lignes discontinues : Pattern
+    pattern = re.compile(r'[\_\_]*')
+    ### Suppression des espaces entres paragraphes : Pattern
+    pattern_1 = re.compile(r'\n(\n+)')
+    ### Suppression des entêtes : Pattern
+    heading_pattern = re.compile(r'^.*?PEUPLE\sFRANÇAIS', re.DOTALL) #supprime toutes les lines du début jusqu'à la fin.
+
+    #Suppression globale
+    mddata = re.sub(page_number_pattern,r'',mddata)
+    mddata = re.sub(pattern,'',mddata)
+    mddata = re.sub(pattern_1,r'\n', mddata)
+    mddata = re.sub(heading_pattern, '', mddata)
+
+    ### Suppression des retours à la lignes
+    pattern = re.compile(r'(?<!\.)\n')
+    mddata = pattern.sub(' ', mddata)
+    
+    ### Saut de lignes
+    pattern = re.compile(r'(?<!\b[A-Z0-9])\.\s*([A-Z])') # Je regarde si y'a pas un chiffre ou une majuscule avant le point (signe que c'est un nom propre ou des bullets points). Ensuite je crée le saut de ligne
+    mddata = pattern.sub(r'.\n\n\1', mddata)
+
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"Pourvoi {NUM.group(1).strip()} du {date.group(1).strip()}") #écriture et extraction
     md.append("")  # Ligne vide
     md.append(mddata)
 
