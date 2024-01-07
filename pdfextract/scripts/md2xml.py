1ceb0c8a5cb2043cb9e6c638bdfc3fb187c0d660
jedupau <dupaujeanedouard@gmail.com>
2024-01-07 16:24:49 +0100

    rendu

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,6 +17,18 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
+    # delimitation section Entête
+    mddata = """<div class="ENTETE">\n"""+mddata
+    # delimitation section Exposé du litige
+    mddata = re.sub('^faits et procédure',r"""</div>\n\n<div class="EXPOSE-DU-LITIGE">\nFaits et procédure""",mddata)
+    # delimitation section Moyens
+    mddata = re.sub('^(Enonc(é|e)|Examen) d((u)|(es)) moyen(|s)',r"""</div>\n\n<div class="MOYENS">\n\1""",mddata)
+    # delimitation section Motivation
+    mddata = re.sub('^R(é|e)ponse de la cour',r"""</div>\n\n<div class="MOTIVATION">\nRéponse de la Cour""",mddata)
+    # delimitation section Dispositif
+    mddata = re.sub('^((par ce(|s) motif(|s))|(en conséquence))',r"""</div>\n\n  <div class="DISPOSITIF">\n\1""",mddata)
+    mddata += "\n  </div>"
+    mddata = re.sub(r"\n([^<>\n]+)\n",r"\n    <p>\n      \1\n    </p>\n",mddata)
     xmldata = mddata
 
     xml = list()
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -14,13 +14,32 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    pourvoi = re.search('(?<=Pourvoi N°)\d+-\d+\.\d+',textdata, re.MULTILINE).group()
+    date = re.search('[0-9]{1,2} \w+ [0-9]{4}',textdata, re.MULTILINE).group()
 
+    # Enleve le pourvoi
+    textdata = re.sub('Pourvoi N°\d+-\d+\.\d+(-[a-zA-ZéÉèÈàÀûÛ\s]+)?(?:\s{0,2}|$)', '',textdata)
+
+    # Enleve la date
+    textdata = re.sub('\s{2}[0-9]{1,2} \w+ [0-9]{4}', '',textdata)
+
+    # Enleve les sauts de page
+    textdata = re.sub(r"\n(\n)+", '\n\n', textdata)
+    
+    # Enleve les barres horizontales
+    textdata = re.sub(r'_{2,}', '', textdata)
+    
+    # Enleve les numéros de page
+    textdata = re.sub(r'\s*Page \d+ / \d+','',textdata)
+
+    # Enleve les retours chariots
+    textdata = re.sub(r'([\.,\w\[\]])\n([a-zA-Z\[\]]|\d{1,2}(?!\.))',r'\1 \2',textdata)
+
+    mddata = textdata
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
-    md.append("")  # Ligne vide
+    
+    md.append(f"# Pourvoi {pourvoi} du {date}")
+    md.append("")
     md.append(mddata)
 
     outdata = '\n'.join(md) 
