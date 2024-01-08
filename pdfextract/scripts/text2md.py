e0b2e316b5be780723d9e5e4a45b7c11fe601468
20sisca <alexis.siscaa@gmail.com>
2024-01-08 09:13:17 +0100

    formatted text + get date

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -16,13 +16,37 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
+    
+    print('AAAAAAAAAAAAAAAA')
+    textdata = format_text(textdata)
+    date = get_date(textdata)
+    print('AAAAAAAAAAAAAAAA')
     mddata = textdata
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi NUM_POURVOI du {date}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
     outdata = '\n'.join(md) 
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
+        
+def format_text(text):
+    text= re.sub(r'\n+', '\n', text)
+    text = re.sub(r'\x0c', '', text)  # Supprimer les caractères de saut de page
+    text = re.sub(r'Page \d+ \/ \d+', '', text)
+    return text
+    
+def get_date(text):
+    # Expression régulière pour trouver une date au format "13 décembre 2023"
+    date_regex = r'\b(\d{1,2}\s+(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)\s+\d{4})\b'
+
+    # Recherche de la première date dans le texte en ignorant la casse
+    match_date = re.search(date_regex, text, re.IGNORECASE)
+    
+    if match_date:
+        first_date = match_date.group(1)
+    else:
+        return "date introuvable"
+    return first_date
