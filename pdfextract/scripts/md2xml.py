50f822036b8033b37a90886bb7b376a2ebf15adf
clariedm <c.demouy19@ejm.org>
2024-01-08 02:55:37 +0100

    TP-fini

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -17,7 +17,44 @@ def main():
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
+    
+    mddata2 = mddata # Dédoublement du texte pour éviter les erreurs d'overlapping
+
+    # Initialisation de la variable xmldata
+    xmldata = str()
+
+    # Définir les motifs permettant de sélectionner chaque partie
+    header_pattern = re.compile(r'^# (.+)$', re.MULTILINE)
+    motivation_pattern = re.compile(r'(N°|CIV.)(.*?)(?:Faits et procédure|(?:1\. ))', re.MULTILINE| re.DOTALL)
+    exposé_pattern = re.compile(r'Faits et procédure(.*?)(?:Examen (?:des moyens|du moyen))', re.MULTILINE | re.DOTALL)
+    moyens_pattern = re.compile(r'Examen (des|du) (moyens|moyen) .*?(?=PAR CES MOTIFS|\n\n)', re.MULTILINE | re.DOTALL)
+    moyens_backup = re.compile(r'1\. (.*?)(?=PAR CES MOTIFS|\n\n)', re.MULTILINE | re.DOTALL)
+    dispositif_pattern = re.compile(r'(PAR CES MOTIFS|EN CONS)(.*?)($)', re.MULTILINE | re.DOTALL)
+
+    # Extraction du contenu de chaque zone
+    header_match = header_pattern.search(mddata)
+    if header_match:
+        xmldata += f'  <header>{header_match.group(0)}</header>\n'
+
+    motivation_match = motivation_pattern.search(mddata)
+    if motivation_match:
+        xmldata += f'  <motivation>{motivation_match.group(0)}</motivation>\n'
+
+    exposé_match = exposé_pattern.search(mddata2)
+    if exposé_match:
+        xmldata += f'  <expose>{exposé_match.group(1)}</expose>\n'
+
+    moyens_match = moyens_pattern.search(mddata)
+    if moyens_match:
+       xmldata += f'  <moyens>{moyens_match.group(0)}</moyens>\n'
+    else:
+        backup_match = moyens_backup.search(mddata)
+        if backup_match: 
+            xmldata += f'  <moyens>{moyens_backup.group(0)}</moyens>\n'
+
+    dispositif_match = dispositif_pattern.search(mddata2)
+    if dispositif_match:
+        xmldata += f'  <dispositif>{dispositif_match.group(0)}</dispositif>\n'
 
     xml = list()
     xml.append('<?xml version="1.0" encoding="utf-8"?>')
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -16,10 +16,57 @@ def main():
 
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
+    
+    # Ajout d'un titre de format 'Pourvoi NUM du DATE'
+    def extract_number_and_date():
+        # Definir le motif dans lequel extraire le nom et la date
+        pattern = r'Pourvoi N°(\d{2}-\d{2}\.\d{3})-.*?(\d{1,2}\s\w+\s\d{4})'
+
+        # Trouver le motif dans le texte
+        match = re.search(pattern, textdata)
+
+        # Extraire le nombre et la date si le motif est trouvé
+        if match:
+            number = match.group(1)
+            date = match.group(2)
+
+            return number, date
+
+        return None, None
+
+    number, date = extract_number_and_date()
+
+    
+    # Suppression des entêtes
+    def retirer_en_tete(text):
+        # Definir le motif de l'entête
+        pattern = r'Pourvoi N°\d{2}-\d{2}\.\d{3}-.*?\d{1,2}\s\w+\s\d{4}'
+
+        # Supprimer toutes les instances du motif
+        text = re.sub(pattern, '', text)
+
+        return text
+    
+    textdata = retirer_en_tete(textdata)
+
+    # Suppression de tous les retours chariot dans un paragraphe
+    textdata = re.sub(r'([^\n])\n([^\n])', r'\1 \2', textdata)
+
+    # Suppression des sauts de page et numéros de page
+    textdata = re.sub(r'Page \d+ \/ \d+', '', textdata)  # Suppression des numéros de page
+    textdata = re.sub(r'[\f]*', '', textdata)  # Supprime les sauts de page
+
+    # Suppression de tous les alinéas
+    textdata = '\n'.join(line.strip() for line in textdata.split('\n'))
+    
+    # Ne garder qu'une seule ligne vide entre deux paragraphes
+    textdata = re.sub(r'\n+', '\n', textdata)
+
+
     mddata = textdata
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi {number} du {date}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
