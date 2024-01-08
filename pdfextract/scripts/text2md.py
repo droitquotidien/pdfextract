da6f3adb8ba7526d078bce2ad633345df65f8444
Alexandre Grenier <grenieralexandre@free.fr>
2024-01-08 10:19:15 +0100

    Update text2md.py

--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -14,15 +14,28 @@ def main():
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
 
-    # Transform textdata with re here
-    # see https://docs.python.org/fr/3/library/re.html
+    textdata=re.sub(r'_','',textdata)
+    textdata=re.sub(r'Page (\d)+ / (\d)+','',textdata)
+    num_pourvoi = re.search(r"(?P<num>\d\d-\d\d.\d\d\d)", textdata)
+    date_pourvoi = re.search(r"DU (?P<date>\d\d \D+ \d\d\d\d)", textdata)
+    textdata=re.sub(r'Pourvoi (.+)\n','',textdata)
+    textdata=re.sub(r'(\t)+','',textdata)
+    textdata=re.sub('( )+',' ',textdata)
+    textdata=re.sub('','',textdata)
+    textdata=re.sub(r'(\n )',r'\n',textdata)
+    textdata=re.sub(r'(\n)+',r'\n',textdata)
+    textdata=re.sub(r'\n(\d)+. ',r'\n',textdata)
+    textdata=re.sub(r'(?<!\n)\n(?!\n)',' ',textdata)
+    textdata=re.sub(r'(\n)+',r'\n',textdata)
+
+
     mddata = textdata
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append(f"# Pourvoi n°{num_pourvoi.group('num')} du {date_pourvoi.group('date')}")
     md.append("")  # Ligne vide
     md.append(mddata)
 
-    outdata = '\n'.join(md) 
+    outdata = '\n'.join(md)
     with open(args.out_file, "w", encoding="utf-8") as f:
         f.write(outdata)
