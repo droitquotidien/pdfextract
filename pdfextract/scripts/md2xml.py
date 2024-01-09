bb3eab4a0cc2b03d214444485064adbe393d4610
chengfenggu2000 <87238075+chengfenggu2000@users.noreply.github.com>
2024-01-09 01:04:14 +0100

    Add functions

--- pdfextract/scripts/md2xml.py
+++ pdfextract/scripts/md2xml.py
@@ -5,6 +5,66 @@ import argparse
 import re
 import sys
 
+def treat_data(xmldata):
+    # Entête
+    paras = xmldata.split('\n')
+    xml = list()
+    xml.append('<?xml version="1.0" encoding="utf-8"?>')
+    xml.append('<root>')
+    Entete = 'None'
+    for p in paras:
+        Entete_search = re.search(r'DE LA COUR DE CASSATION',p)
+        if Entete_search is not None:
+            Entete = p
+            break
+    xml.append('<Entete>')
+    xml.append(Entete)
+    xml.append('</Entete>')
+
+    # Exposé du litige
+    Expose = 'None'
+    for p in paras:
+        Expose_search = re.search(r'a formé le pourvoi',p)
+        if Expose_search is not None:
+            Expose = p
+            break
+    xml.append('<Expose>')
+    xml.append(Expose)
+    xml.append('</Expose>')
+
+    # Motivation
+    Motif = 'None'
+    for p in paras:
+        Motif_search = re.search(r'Sur le rapport',p)
+        if Motif_search is not None:
+            Motif = p
+            break
+    xml.append('<Motif>')
+    xml.append(Motif)
+    xml.append('</Motif>')
+
+    # Moyens
+    Moyens = 'None'
+    Moyens_search = re.search(r'\d\.\s(.*?EN CONSÉQUENCE)',xmldata,re.DOTALL)
+    if Moyens_search is not None:
+        Moyens = Moyens_search.group(0)
+    xml.append('<Moyens>')
+    xml.append(Moyens)
+    xml.append('</Moyens>')
+
+    # Dispositif
+    Dispositif = 'None'
+    Disp_search = re.search(r'EN CONSÉQUENCE.*\.',xmldata,re.DOTALL)
+    if Disp_search is not None:
+        Dispositif = Disp_search.group(0)
+    xml.append('<Disposition>')
+    xml.append(Dispositif)
+    xml.append('</Disposition>')
+    xml.append('</root>')
+
+    return xml
+    
+
 def main():
     parser = argparse.ArgumentParser("Markdown to XML")
     parser.add_argument('in_file', help="Markdown file")
@@ -12,19 +72,12 @@ def main():
     args = parser.parse_args()
 
     with open(args.in_file, "r", encoding="utf-8") as f:
-        mddata = f.read()
+        xmldata = f.read()
 
     # Transform mddata with re here
     # see https://docs.python.org/fr/3/library/re.html
     # document must be a valid XML file
-    xmldata = mddata
-
-    xml = list()
-    xml.append('<?xml version="1.0" encoding="utf-8"?>')
-    xml.append('<decision>')
-    xml.append(xmldata)
-    xml.append('</decision>')
-
+    xml = treat_data(xmldata)
     outdata = '\n'.join(xml)
 
     with open(args.out_file, "w", encoding="utf-8") as f:
--- pdfextract/scripts/text2md.py
+++ pdfextract/scripts/text2md.py
@@ -4,6 +4,28 @@ Transforme un document texte issu d'un PDF de la cours de cassation en Markdown.
 import argparse
 import re
 
+def clean_text(input_text):
+        cleaned_text = re.sub(r'.*_\n+', '', input_text, flags=re.DOTALL)
+        cleaned_text = re.sub(r'\f', '', cleaned_text)
+        cleaned_text = re.sub(r'.*Page \d{1,2} / \d{1,2}.*', '', cleaned_text)
+        cleaned_text = re.sub(r'Pourvoi.*?(\d{1,2}\s\w+\s\d{4}).*\n', '', cleaned_text)
+        cleaned_text = ' '.join(cleaned_text.split())
+        cleaned_text = re.sub(r'(\b\d{1,2}\b\. )', r'\n\n\1', cleaned_text)
+        
+        pattern = re.compile(r'(\.\s[^\.\n]+\n)')
+        matches = pattern.findall(cleaned_text)
+        
+        for _ in matches:
+            try:
+                pattern = re.escape(_)
+                head = re.search(r'[a-zA-Z]', _)
+                pos = head.start()
+                rst = _[:pos]+"\n\n"+_[pos:]
+                cleaned_text = re.sub(pattern, rst, cleaned_text)
+            except Exception as e:
+                print(e)
+                
+        return cleaned_text
 
 def main():
     parser = argparse.ArgumentParser("text2md")
@@ -13,13 +35,14 @@ def main():
 
     with open(args.in_file, "r", encoding="utf-8") as f:
         textdata = f.read()
-
+    path = f.name
+    
     # Transform textdata with re here
     # see https://docs.python.org/fr/3/library/re.html
-    mddata = textdata
+    mddata = clean_text(textdata)
 
     md = list()
-    md.append("# Pourvoi NUM_POURVOI du DATE")
+    md.append("# Pourvoi "+path[14:25]+" du "+path[26:36])
     md.append("")  # Ligne vide
     md.append(mddata)
 
